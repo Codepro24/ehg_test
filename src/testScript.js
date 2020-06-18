4dfba7b442194fc75c8f208b537438363ac057ae
Prateek Chatterjee <prateekchatterjee@Prateeks-MacBook-Air.local>
2020-06-18 22:29:30 +1000

    added the vanilla javascript file testScript.js

---
+++ src/testScript.js
@@ -0,0 +1,65 @@
+const ctx = document.querySelector('canvas').getContext('2d');
+const imageData = ctx.createImageData(256, 128);
+const data = imageData.data;
+
+const availableColors = initColors();
+
+for (let i = 0; i < data.length; i += 4) {
+
+  let redIndex, blueIndex, greenIndex;
+
+  do {
+
+    redIndex = Math.floor( Math.random() * 32 );
+    blueIndex = Math.floor( Math.random() * 32 );
+    greenIndex = Math.floor( Math.random() * 32 );
+
+  } while ( availableColors[redIndex][blueIndex][greenIndex] === false )
+
+  // availableColors[redIndex][blueIndex][greenIndex] = false;
+
+  data[i + 0] = redIndex * 8;
+  data[i + 1] = blueIndex * 8
+  data[i + 2] = greenIndex * 8;
+  data[i + 3] = 255;
+
+}
+
+ctx.putImageData(imageData, 0, 0);
+
+function initColors() {
+
+  const colors = new Array(32);
+
+  for (let r=0; r < 32; r++) {
+    colors[r] = new Array(32);
+    for (let g=0; g < 32; g++) {
+      colors[r][g] = new Array(32);
+      for (let b=0; b < 32; b++) {
+        colors[r][g][b] = true;
+      }
+    }
+  }
+  return colors;
+}
+
+  redIndex = Math.floor(i/8) % 32;
+  blueIndex = Math.floor(i/16) % 32;
+  greenIndex = Math.floor(i/24) % 32;
+
+// A program which will produce an image in which each colour occurs exactly once -- with no repetition and no used colours. 
+// 1st step: Let's just produce an image in which each colour occurs exactly once, with no repetition.
+ctx.fillRect();
+ctx.fillStyle();
+// Colours are formed by combining a red, green, and blue component in the range 0..256;
+// Your program will need to break each component into 32 steps -- 8, 16,24, .. 256 -- which means you will have 32,768 discrete colours. 
+// 2nd step: each component meaning red broken into 32 steps, blue into 32, and green into 32
+// or red (0..255) -> 7, 15, 23...
+// or r32*g32*b32
+
+let gradient = ctx.createLinearGradient(0, 0, 200, 0);
+gradient.addColorStop(0, 'green');
+gradient.addColorStop(.7, 'white');
+gradient.addColorStop(1, 'pink');
+ctx.fillStyle = gradient;
+ctx.fillRect(10, 10, 200, 100);
