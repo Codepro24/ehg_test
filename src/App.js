5a5cb0122eef65848361fd1efb4ece17f72fd3f9
Prateek Chatterjee <prateekchatterjee@Prateeks-MacBook-Air.local>
2020-06-20 19:58:46 +1000

    got some code from mdn

--- src/App.js
+++ src/App.js
@@ -3,51 +3,49 @@ import React from 'react';
 class App extends React.Component {
     
     componentDidMount(){
-        this.updateCanvas();
+     console.log("It works!");
     }
-    updateCanvas() {
+    updateCanvas = () => {
         const ctx = this.refs.canvas.getContext('2d');
         let imgData = ctx.createImageData(256, 128);
         
-        //ctx.fillStyle = 'rgb(0,0,255)';
-        // ctx.fillRect(10,10,256,128);
-        //this.initColors();
         console.log(imgData.data.length);
-        var rShade, gShade, bShade;
+        //var rShade, gShade, bShade;
         //this.initColors();
         for (let i = 0; i < imgData.data.length; i += 4) {
-            const initColors = ({rShade, gShade, bShade}) => {
-                let count=0;
-                const colors = new Array(32);
-                for (let r=0; r < 256; r+=8) {
-                    colors[r] = new Array(32);
-                    for (let g=0; g < 256; g+=8) {
-                        colors[r][g] = new Array(32);
-                        for (let b=0; b < 256; b+=8) {
-                            colors[r][g][b] = [r,g,b];
-                            rShade = r;
-                            gShade = g;
-                            bShade = b;
-                            count++;
-                            console.log(colors[r][g][b], count);
-                        }
-                    }
-                }
-                return colors;
-            }
-
-            imgData.data[i+0] = rShade; //r value
-            imgData.data[i+1] = gShade;  //g value
-            imgData.data[i+2] = bShade;  //b value
+            imgData.data[i+0] = i+0; //r value
+            imgData.data[i+1] = i+1;  //g value
+            imgData.data[i+2] = i+2;  //b value
             imgData.data[i+3] = 255;  //opacity
+            // const initColors = ({rShade, gShade, bShade}) => {
+            //     let count=0;
+            //     const colors = new Array(32);
+            //     for (let r=0; r < 256; r+=8) {
+            //         colors[r] = new Array(32);
+            //         for (let g=0; g < 256; g+=8) {
+            //             colors[r][g] = new Array(32);
+            //             for (let b=0; b < 256; b+=8) {
+            //                 colors[r][g][b] = [r,g,b];
+            //                 rShade = r;
+            //                 gShade = g;
+            //                 bShade = b;
+            //                 count++;
+            //                 console.log(colors[r][g][b], count);
+            //             }
+            //         }
+            //     }
+            //     return colors;
+            // }
+
+            
           }
           //draw image data to the canvas
-          ctx.putImageData(imgData, 10, 10);
+          ctx.putImageData(imgData, 10,10);
     }  
         
     render() {
         return (
-            <div>
+            <div style={{border: "1px solid black"}} onMouseEnter={this.updateCanvas}>
                <canvas ref="canvas"></canvas>
             </div>
             );
---
+++ src/extraCode.js
@@ -0,0 +1,2 @@
+ctx.fillStyle = 'rgb(0,0,255)';
+ctx.fillRect(10,10,256,128);
