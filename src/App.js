61c31c3f05a9ef3fb1085dd2111973d2c4bb4ea8
Prateek Chatterjee <prateekchatterjee@Prateeks-MacBook-Air.local>
2020-06-22 00:15:50 +1000

    final project

--- src/App.js
+++ src/App.js
@@ -1,65 +1,87 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
-    
+    state = {
+        useRandom: true
+    }
     componentDidMount(){
-     console.log("It works!");
+        this.updateCanvas();
     };
-    //this function has the logic
+
+    //this creates the Canvas context
     updateCanvas = () => {
         const ctx = this.refs.canvas.getContext('2d');
         let imgData = ctx.createImageData(256, 128);
-        console.log(imgData.data.length); //131072 which is 32728 times 4
-
-        //the array of colors, non-repeating and in intervals of 8 for r, g, b
-        const colors = new Array(32);
-                for (let r=0; r < 256; r+=8) {
-                    colors[r] = new Array(32);
-                    for (let g=0; g < 256; g+=8) {
-                        colors[r][g] = new Array(32);
-                        for (let b=0; b < 256; b+=8) {
-                            colors[r][g][b] = [r,g,b];
-                            //count++;
-                            console.log(colors[r][g][b]);
-                        }
-                    }
-                }
-        //declare three variables to hold current r,g,b values and multiply their random value by 32  
-        //because we need 32 components of each colour out of their 256 values (0...255)        
-        let rcurrentVal, gcurrentVal, bcurrentVal;
-        rcurrentVal = Math.floor(Math.random() * 32);
-        gcurrentVal = Math.floor(Math.random() * 32);
-        bcurrentVal = Math.floor(Math.random() * 32);
-                
-        var currentColor = [];
-        
-        function checkForRepeats(color) {
-            if ( color !== currentColor(rcurrentVal,gcurrentVal,bcurrentVal) )
-                return currentColor(rcurrentVal,gcurrentVal,bcurrentVal);
+        const data = imgData.data;
+        if (this.state.useRandom) {
+            this.randomPixelData(data);
+        } else {
+            this.gradientPixelData(data);
         }
-        
-        // eslint-disable-next-line no-array-constructor
-        //const colorVal = new Array();
-        
-        for (let i = 0; i < imgData.data.length; i += 4) {
-            colors.every(checkForRepeats);
-                imgData.data[i+0] = rcurrentVal; //r value
-                imgData.data[i+1] = gcurrentVal;  //g value
-                imgData.data[i+2] = bcurrentVal;  //b value
-          
-            
-          };
-          //draw image data to the canvas
-          ctx.putImageData(imgData, 10,10);
-    };  
-        
+        ctx.putImageData(imgData, 0, 0);
+    };
+    
+    initColors() {
+        const colors = new Array(32);
+        for (let r=0; r < 32; r++) {
+            colors[r] = new Array(32);
+            for (let g=0; g < 32; g++) {
+                colors[r][g] = new Array(32);
+                for (let b=0; b < 32; b++) {
+                    colors[r][g][b] = true;
+                }
+            }   
+        }
+        return colors;
+    }
+    
+    randomPixelData(data){
+        const availableColors = this.initColors();
+        for (let i = 0; i < data.length; i += 4) {
+            let redIndex, blueIndex, greenIndex;
+            do {
+                redIndex = Math.floor( Math.random() * 32 );
+                blueIndex = Math.floor( Math.random() * 32 );
+                greenIndex = Math.floor( Math.random() * 32 );
+            } while ( availableColors[redIndex][blueIndex][greenIndex] === false )
+            availableColors[redIndex][blueIndex][greenIndex] = false;
+            data[i + 0] = redIndex * 8;
+            data[i + 1] = blueIndex * 8
+            data[i + 2] = greenIndex * 8;
+            data[i + 3] = 255;
+        }
+    }
+    gradientPixelData(data){
+        let index = 0;
+        for (let red = 0; red < 256; red+=8) {
+            for (let green = 0; green < 256; green+=8) {
+                for (let blue = 0; blue < 256; blue+=8) {
+                    data[index + 0] = red;
+                    data[index + 1] = green;
+                    data[index + 2] = blue;
+                    data[index + 3] = 255;
+                    index+=4;
+                }
+            }
+        }
+    }
+
+    toggleRender = async () => {
+        await this.setState({
+            useRandom: !this.state.useRandom
+        });
+        this.updateCanvas();
+    }
+    
     render() {
         return (
-            <div style={{border: "1px solid black"}} onMouseEnter={this.updateCanvas}>
-               <canvas ref="canvas"></canvas>
+            <div id="mainDiv" className="ui raised container center aligned segment">
+                <h1 className="ui header">Fun with Colours!</h1>
+                <canvas ref="canvas" onClick={this.toggleRender}></canvas>
+                <p>Click the above image to change patterns</p>
             </div>
-            );
+        );
     };
 }
-    
 export default App;
--- src/extraCode.js
+++
@@ -1,2 +0,0 @@
-//ctx.fillStyle = 'rgb(0,0,255)';
-//ctx.fillRect(10,10,256,128);
